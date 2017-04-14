4097a0581e5e82f1124c5108fc260b68225bb846
Anton Goncharuk <antonge92@gmail.com>
2017-04-15 02:37:32 +0300

    Implement smooth scrolling when navigate between blocks using header links.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,12 +3,30 @@
 
   const $iphoneImgSmall = $('.js-iphone-img-small');
   const $iphoneImgBig = $('.js-iphone-img-big');
+  const $navLink = $('.js-nav-link');
+  const $root = $('html, body');
 
   init();
 
   function init () {
     $iphoneImgSmall.addClass('is-shown');
     $iphoneImgBig.addClass('is-shown');
+    $navLink.on('click', handleNavigation);
+    // $(window).on('scroll', handleScroll);
   }
+
+  function handleNavigation (e) {
+    e.preventDefault();
+
+    const href = $.attr(this, 'href');
+
+    $root.animate({
+      scrollTop: $(href).offset().top
+    }, 500, () => {
+      window.location.hash = href;
+    });
+  }
+
+  // function handleScroll () {}
 
 })(jQuery);
