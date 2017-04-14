e970592cbb502fe3dbca10483b94c9bfba8d36f8
Anton Goncharuk <antonge92@gmail.com>
2017-04-15 02:23:43 +0300

    Implement script to fire off animation on page load. Update intro styles. Apply animations. Small updates.

---
+++ app/scripts/main.js
@@ -0,0 +1,14 @@
+(function ($) {
+  'use strict';
+
+  const $iphoneImgSmall = $('.js-iphone-img-small');
+  const $iphoneImgBig = $('.js-iphone-img-big');
+
+  init();
+
+  function init () {
+    $iphoneImgSmall.addClass('is-shown');
+    $iphoneImgBig.addClass('is-shown');
+  }
+
+})(jQuery);
